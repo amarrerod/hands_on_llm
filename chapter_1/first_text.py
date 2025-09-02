bcad25f31edefb6d73eb3800d014769253dea4ed
amarrerod <alemarrerodiaz@gmail.com>
2025-09-02 11:17:25 +0100

    🎉 Starts the project

---
+++ chapter_1/first_text.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+"""
+@File    :   first_text.py
+@Time    :   2025/09/02 11:06:18
+@Author  :   Alejandro Marrero
+@Version :   1.0
+@Contact :   amarrerd@ull.edu.es
+@License :   (C)Copyright 2025, Alejandro Marrero
+@Desc    :   None
+"""
+
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+
+if __name__ == "__main__":
+    model_name = "BSC-LT/salamandra-7b-instruct"
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name, device_map="cuda", torch_dtype="auto", trust_remote_code=True
+    )
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    pipeline = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        return_full_text=False,
+        max_new_token=500,
+        do_sample=False,
+    )
+    messages = [{
+        "role": "user",
+        "content": "Crea un chiste sobre gallinas en Español y Galego."
+    }]
+    output = pipeline(messages)
+    print(output)
+    print(f"Resultado: \n {output[0]['generated_text']}")
