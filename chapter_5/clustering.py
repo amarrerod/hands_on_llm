5bf98df141c2367ac2fef232513bca720f1e2c2c
amarrerod <alemarrerodiaz@gmail.com>
2025-09-04 14:37:35 +0100

    🚧 Working on chapter 5

---
+++ chapter_5/clustering.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+"""
+@File    :   clustering.py
+@Time    :   2025/09/04 13:30:47
+@Author  :   Alejandro Marrero
+@Version :   1.0
+@Contact :   amarrerd@ull.edu.es
+@License :   (C)Copyright 2025, Alejandro Marrero
+@Desc    :   None
+"""
+
+from datasets import load_dataset
+from sentence_transformers import SentenceTransformer
+import pandas as pd
+from umap import UMAP
+from hdbscan import HDBSCAN
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+if __name__ == "__main__":
+    dataset = load_dataset("maartengr/arxiv_nlp")["train"]
+    abstracts, titles = dataset["Abstracts"], dataset["Titles"]
+    embd_model = SentenceTransformer(
+        "thenlper/gte-small"
+    )  # Qwen/Qwen3-Embedding-4B Works better
+    embds = embd_model.encode(abstracts, show_progress_bar=True)
+
+    print(embds.shape)
+    umap_model = UMAP(n_components=2, metric="cosine", random_state=42)
+    reduced_embds = umap_model.fit_transform(embds)
+
+    hdbscan_model = HDBSCAN(
+        min_cluster_size=50, metric="euclidean", cluster_selection_method="eom"
+    ).fit(reduced_embds)
+
+    clusters = hdbscan_model.labels_
+    print(f"{len(set(clusters))} clusters created.")
+    df = pd.DataFrame(reduced_embds, columns=["x0", "x1"])
+    df["title"] = titles
+    df["cluster"] = [str(c) for c in clusters]
+
+    sns.scatterplot(df[df.cluster == -1], x="x0", y="x1", alpha=0.05, s=2, c="grey")
+    sns.scatterplot(
+        df[df.cluster != -1],
+        x="x0",
+        y="x1",
+        hue="cluster",
+        alpha=0.6,
+        s=2,
+        cmap="tab20b",
+    )
+    plt.show()
